e3f27cfa02e413d041b23472a68c8a4144170123
Andrés Muro <andres.muro97@gmail.com>
2020-02-16 21:15:51 -0600

    Creadas 3 pruebas para el modelo Articulo

--- hola/tests.py
+++ hola/tests.py
@@ -1,5 +1,38 @@
 from django.test import TestCase
+from hola.models import Articulo 
+from django.db import IntegrityError
 
 class TestSmoke(TestCase):
     def test_smoke_test(self):
         self.assertEqual(2 + 2, 4)
+
+class BaseModelTest(TestCase):
+
+    def test_agrega_articulo(self):
+                articulo = Articulo.objects.create(
+                    nombre = 'Xbox',
+                    precio = 321.30,
+                    descripcion = 'Consola de juegos'
+                )
+                articulo_uno = Articulo.objects.first()
+
+                self.assertEqual(articulo, articulo_uno)
+                self.assertEqual(str(articulo_uno), 'Xbox')
+        
+    def test_articulo_nombre_null(self):
+            with self.assertRaises(IntegrityError):
+                articulo = Articulo.objects.create(
+                    nombre = None,
+                    precio = 321.30,
+                    descripcion = 'Consola de juegos'
+                )
+                base.full_clean()   
+    
+    def test_articulo_precio_null(self):
+            with self.assertRaises(IntegrityError):
+                articulo = Articulo.objects.create(
+                    nombre = "Xbox",
+                    precio = None,
+                    descripcion = 'Consola de juegos'
+                )
+                base.full_clean()  
